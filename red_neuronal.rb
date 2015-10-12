b0a7488134450111ef9b9487dd95d38a651287fe
Hernan Maschwitz <hernanm992@gmail.com>
2015-10-12 17:05:52 -0300

    add information and configuration

--- red_neuronal.rb
+++ red_neuronal.rb
@@ -1,18 +1,20 @@
 require "rubygems"
 require "ai4r"
 
-CAREER = ['Ingeniero:', 'Abogado:  ', 'Medico:   ', 'Diseñador:']
+CAREER = ['Ingeniero:', 'Abogado:  ', 'Medico:   ', 'Diseñador:', 'Contador: ']
 skills = ARGV[0][1..-2].split(',').collect! {|n| n.to_i}
 
 def result_label(result) #Match te optimal result
-  if result[0] > result[1] && result[0] > result[2] && result[0] > result[3]
+  if result[0] > result[1] && result[0] > result[2] && result[0] > result[3] && result[0] > result[4]
     return "Ingeniero"
-  elsif result[1] > result[2] && result[1] > result[3]
+  elsif result[1] > result[2] && result[1] > result[3] && result[1] > result[4]
     return "Abogado"
-  elsif result[2] > result[3]   
+  elsif result[2] > result[3] && result[2] > result[4]
     return "Medico"
-  else
+  elsif result[3] > result[4]
   	return "Diseñador"
+  else
+    return "Contador"
   end
 end
 
@@ -25,28 +27,32 @@ def result_values(result)
 end
 
 #Create the network with 4 inputs, 1 hidden layer with 5 neurons, and 4 outputs
-net = Ai4r::NeuralNetwork::Backpropagation.new([4, 10, 4])
+net = Ai4r::NeuralNetwork::Backpropagation.new([5, 10, 10, 5])
 
 #Initialize values
-#net.momentum = 0.15 
-#net.learning_rate = 0.5
-#net.propagation_function = lambda { |x| Math.tanh(x) }  
-#PONER VALORES DEFAULT
+net.momentum = 0.1
+net.learning_rate = 0.1
+net.propagation_function = lambda { |x| 1/(1+Math.exp(-1*(x))) }
 
 # Train the network 
-1000.times do |i|
-  net.train([0, 1, 0, 0], [1, 0, 0, 0]) #Ingeniero
-  net.train([0, 0, 1, 0], [1, 0, 0, 0])
+5000.times do |i|
+  net.train([0, 1, 0, 0, 0], [1, 0, 0, 0, 0]) #Ingeniero
+  net.train([0, 0, 1, 0, 0], [1, 0, 0, 0, 0])
+  net.train([0, 0, 0, 0, 1], [1, 0, 0, 0, 0])
 
-  net.train([0, 0, 1, 0], [0, 1, 0, 0]) #Abogado
-  net.train([0, 0, 0, 1], [0, 1, 0, 0])
+  net.train([0, 0, 1, 0, 0], [0, 1, 0, 0, 0]) #Abogado
+  net.train([0, 0, 0, 1, 0], [0, 1, 0, 0, 0])
+  net.train([0, 0, 0, 0, 1], [0, 1, 0, 0, 0])
 
-  net.train([0, 1, 0, 0], [0, 0, 1, 0]) #Medico
-  net.train([0, 0, 0, 1], [0, 0, 1, 0])
+  net.train([0, 1, 0, 0, 0], [0, 0, 1, 0, 0]) #Medico
+  net.train([0, 0, 0, 1, 0], [0, 0, 1, 0, 0])
 
-  net.train([1, 0, 0, 0], [0, 0, 0, 1]) #Diseñador
+  net.train([1, 0, 0, 0, 0], [0, 0, 0, 1, 0]) #Diseñador
+
+  net.train([0, 1, 0, 0, 0], [0, 0, 0, 0, 1]) #Contador
+  net.train([0, 0, 0, 0, 1], [0, 0, 0, 0, 1]) 
 end
-#podes poner 0.3 de limite
+
 
 #Evaluate data with the trained network
 #puts skills
@@ -54,23 +60,6 @@ result_values(net.eval(skills))
 puts "Resultado optimo: #{result_label(net.eval(skills))}"
 
 
-#creativo (1), analitico (2), trabajo en equipo (3), facilidad para expresarse (4)
-#ingeniero (1), abogado (2), medico (3), diseñador (4)
-
-
-
-
-
-#Como le doy los pesos?
-#init_weights()
-
-
-
-#Configurable momentum: 0.1
-#
-#Configurable learning rate: 0.25
-#
-#Configurable initial weight function:
-#
-#Propagation function: lambda { |x| 1/(1+Math.exp(-1*(x))) }
+#creativo (1), analitico (2), trabajo en equipo (3), facilidad para expresarse (4), facilidad de negocioacion (5)
+#ingeniero (1), abogado (2), medico (3), diseñador (4), contador (5)
 
